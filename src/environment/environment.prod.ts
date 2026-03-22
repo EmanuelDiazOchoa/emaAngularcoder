c92a54f0e9af84edd91fed89bc341a9c5bf3cc20
Emanuel Diaz Ochoa <emagnr93@gmail.com>
2026-03-22 02:49:07 -0300

    feat: terminado

---
+++ src/environment/environment.prod.ts
@@ -0,0 +1,4 @@
+export const environment = {
+  production: true,
+  apiUrl: 'https://centro-estudiantes-api.onrender.com'
+};
--- src/environment/environment.ts
+++ src/environment/environment.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: false,
-  apiUrl: 'https://centro-estudiantes-api.onrender.com'
+  apiUrl: 'http://localhost:3000'
 };
