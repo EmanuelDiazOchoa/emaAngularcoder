15fec91f4978da3964820eb15b04b2054402fa69
Emanuel Diaz Ochoa <emagnr93@gmail.com>
2026-03-25 13:05:03 -0300

    feat: corrections v 1.0

--- src/app/core/layout/main-layout/main-layout.component.ts
+++ src/app/core/layout/main-layout/main-layout.component.ts
@@ -1,10 +1,18 @@
 import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+import { NavbarComponent } from '../navbar/navbar.component';
+import { SidebarComponent } from '../sidebar/sidebar.component';
 
 @Component({
   selector: 'app-main-layout',
   standalone: true,
   templateUrl: './main-layout.component.html',
   styleUrls: ['./main-layout.component.scss'],
+  imports: [
+    RouterOutlet,    
+    NavbarComponent,  
+    SidebarComponent  
+  ]
 })
 export class MainLayoutComponent {
 
