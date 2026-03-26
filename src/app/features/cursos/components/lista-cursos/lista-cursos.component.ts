56b35a7315a9b9bd01b8266124745712e6cefe16
Emanuel Diaz Ochoa <emagnr93@gmail.com>
2026-03-26 11:45:16 -0300

    feat: corrections v 1.3

--- src/app/features/cursos/components/lista-cursos/lista-cursos.component.ts
+++ src/app/features/cursos/components/lista-cursos/lista-cursos.component.ts
@@ -45,7 +45,7 @@ export class ListaCursosComponent implements OnInit {
   error$: Observable<string | null> = this.store.select(selectCursosError);
   isAdmin$: Observable<boolean> = this.store.select(selectIsAdmin);
 
-  columnas = ['id', 'nombre', 'descripcion', 'duracion', 'clases', 'profesor', 'fechas', 'acciones'];
+  columnas = ['id', 'nombre', 'duracion', 'clases', 'profesor', 'fechas', 'acciones'];
   isMobile = false;
 
   ngOnInit(): void {
